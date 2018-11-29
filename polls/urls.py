bcbad1bb2d62a793e530930e18b59a900c2e79d2
ML expert <vasile123andronic@gmail.com>
2018-11-29 10:25:39 -0800

    views

--- polls/urls.py
+++ polls/urls.py
@@ -8,8 +8,4 @@ urlpatterns = [
 	path('/<int:pk>/', views.DetailView.as_view(), name='detail'),
 	path('/<int:pk>/results/', views.ResultsView.as_view(), name='results'),
 	path('/<int:question_id>/vote/', views.vote, name='vote'),
-	path('articles/2003/', views.special_case_2003),
-    path('articles/<int:year>/', views.year_archive),
-    path('articles/<int:year>/<int:month>/', views.month_archive),
-    path('articles/<int:year>/<int:month>/<slug:slug>/', 
 ]
